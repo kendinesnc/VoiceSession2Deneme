f2d2858e05fa4a457e870448c549b80c597424db
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-11-09 05:40:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,26 @@
 from os import getenv
-
+import os
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
+que = {}
+SESSION_NAME1 = getenv("SESSION_NAME1", "session1")
+SESSION_NAME2 = getenv("SESSION_NAME2", "session2")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-BOT_USERNAME = getenv("BOT_USERNAME")
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-
-PMPERMIT = getenv("PMPERMIT", None)
-
-API_ID = int(getenv("API_ID"))
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DaisyXupdates")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
+admins = {}
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
-
+BOT_USERNAME = getenv("BOT_USERNAME") 
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Mehmetbaba55")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split())) 
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
