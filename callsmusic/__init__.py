18a3eac6be878c0880574221439596a94f82d88b
King Mehmet <82456249+Mehmetbaba55@users.noreply.github.com>
2021-11-09 05:35:07 +0300

    Update __init__.py

--- callsmusic/__init__.py
+++ callsmusic/__init__.py
@@ -1,2 +1,10 @@
-from .callsmusic import pytgcalls, run
-from . import queues
+from pyrogram import Client
+from pyrogram import idle
+
+from import config
+
+client1 = Client(config.SESSION_NAME1, config.API_ID1, config.API_HASH1)
+client2 = Client(config.SESSION_NAME2, config.API_ID2, config.API_HASH2)
+
+run1 = client1.run1
+run2 = client2.run2 
